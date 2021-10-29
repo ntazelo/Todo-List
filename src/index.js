79cf50242c86a31896775ca861d2f011bcd948c2
ntazelo <ntazelo@gmail.com>
2021-10-29 11:41:02 +0200

    Add checkbox functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
+import Update from './completed.js';
 import './style.css';
 
 class TodoDis {
   static populateTodos() {
     const todos = [
-      { description: 'doing some errand', completed: true, index: 0 },
-      { description: 'meeting with my friends', completed: true, index: 1 },
-      { description: 'learning to play piano', completed: true, index: 2 },
+      { description: 'doing some errand', completed: false, index: 0 },
+      { description: 'meeting with my friends', completed: false, index: 1 },
+      { description: 'learning to play piano', completed: false, index: 2 },
     ];
+
+    let todosloc;
+    if (localStorage.getItem('data')) {
+      todosloc = JSON.parse(localStorage.getItem('data'));
+    } else {
+      localStorage.setItem('data', JSON.stringify(todos));
+      todosloc = JSON.parse(localStorage.getItem('data'));
+    }
+
     const itemslist = document.querySelector('.cont-todos');
-    todos.forEach((todos) => {
+    todosloc.forEach((todos) => {
       const itemlist = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.className = 'check';
+      checkbox.id = todos.index;
       checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = todos.completed;
       const todoField = document.createElement('input');
       todoField.className = 'todo-edt-input';
+      if (todos.completed === true) {
+        todoField.style.textDecoration = 'line-through';
+      }
       todoField.setAttribute('type', 'text');
       const iconSpan = document.createElement('span');
       iconSpan.className = 'symbol-option-cont';
@@ -30,4 +45,7 @@ class TodoDis {
   }
 }
 
-document.addEventListener('DOMContentLoaded', TodoDis.populateTodos());
+document.addEventListener('DOMContentLoaded', () => {
+  TodoDis.populateTodos();
+  Update.checked();
+});
